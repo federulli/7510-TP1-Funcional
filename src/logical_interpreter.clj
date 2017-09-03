a5e10755092a603aa3abec032cd619c692bff297
Federico Rulli <fede.rulli@hotmail.com>
2017-09-03 15:18:57 -0300

    aproximacion

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -17,14 +17,69 @@
 ")
 
 
+(defn replace-map
+      "given an input string and a hash-map, returns a new string with all
+      keys in map found in input replaced with the value of the key"
+      [s m]
+      (clojure.string/replace s
+                              (re-pattern (apply str (interpose "|" (map #(java.util.regex.Pattern/quote %) (keys m)))))
+                              m))
+
+
+(defn get_facts_list [facts_string variables_map]
+      (def splited_facts (clojure.string/split facts_string #"(?<![A-Z]),"))
+      (into [] (map create_fact_or_rule (map #(replace-map % variables_map ) splited_facts))
+            ))
+
 
 (defrecord Fact [name variables])
 
 (defrecord Rule [name variables facts])
 
 (defmulti match_to (fn [this other] [(class this) (class other)]))
+
 (defmethod match_to [Fact Fact] [this other] (= this other))
-(defmethod match_to [Fact Rule] [this other] false)
+
+(defmethod match_to [Fact Rule] [this other]
+           (if (not= (:name this) (:name other))
+             false
+             (if (not= (count (:variables this)) (count (:variables other)))
+               false
+               (are_all_facts_true this other)
+               )
+             )
+           )
+
+(defn is_the_fact_true [fact list_of_facts_and_rules]
+      (if (= nil (some #(match_to fact %) list_of_facts_and_rules))
+        false
+        true
+        )
+      )
+
+(defn are_all_facts_true [fact rule]
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      (def database "varon(juan).
+  	varon(pepe).
+  	varon(hector).
+  	varon(roberto).
+  	varon(alejandro).
+  	mujer(maria).
+  	mujer(cecilia).
+  	padre(juan, pepe).
+  	padre(juan, pepa).
+  	padre(hector, maria).
+  	padre(roberto, alejandro).
+  	padre(roberto, cecilia).
+  	hijo(X, Y) :- varon(X), padre(Y, X).
+  	hija(X, Y) :- mujer(X), padre(Y, X).")
+      (def list_facts_and_rules (clojure.string/split-lines database))
+      (def list_facts_and_rules (map remove_tabs_dots_and_white_spaces list_facts_and_rules))
+      (def list_facts_and_rules (map create_fact_or_rule list_facts_and_rules))
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      (def facts (get_facts_list (:facts rule) (zipmap (:variables rule) (:variables fact))))
+      (every? #(= true %) (map #(is_the_fact_true % list_facts_and_rules) facts))
+      )
 
 (defn remove_tabs_dots_and_white_spaces[rule_or_fact]
       (clojure.string/replace (clojure.string/replace rule_or_fact #"[\t.]" "") " " "")
@@ -46,7 +101,7 @@
         )
       )
 
-(defn get_facts [string_rule]
+(defn get_facts_string [string_rule]
       (def facts (get (re-find #":-(.*)" string_rule) 1))
       (if (= nil facts)
         (throw (Exception. "Invalid Rule"))
@@ -58,7 +113,7 @@
 (defn create_fact_or_rule [fact_or_rule]
       (if (= nil (re-find #":-" fact_or_rule))
         (new Fact (get_name fact_or_rule) (get_variables fact_or_rule))
-        (new Rule (get_name fact_or_rule) (get_variables fact_or_rule) (get_facts fact_or_rule))
+        (new Rule (get_name fact_or_rule) (get_variables fact_or_rule) (get_facts_string fact_or_rule))
         )
       )
 
@@ -68,7 +123,17 @@
       (def list_facts_and_rules (clojure.string/split-lines database))
       (def list_facts_and_rules (map remove_tabs_dots_and_white_spaces list_facts_and_rules))
       (def list_facts_and_rules (map create_fact_or_rule list_facts_and_rules))
-      (some #(match_to (create_fact_or_rule query) %) list_facts_and_rules)
+      (if (= nil (some #(match_to (create_fact_or_rule query) %) list_facts_and_rules))
+        false
+        true
+        )
       )
 
-(evaluate-query parent-database "mujer(maria)")
+;(evaluate-query parent-database "hijo(pepe, juan)")
+(def factstring "hija(cecilia,roberto)")
+(def fact (new Fact (get_name factstring) (get_variables factstring)))
+(def rulestring "hija(X,Y):-mujer(X),padre(Y,X)")
+(def rule (new Rule(get_name rulestring) (get_variables rulestring) (get_facts_string rulestring)))
+
+
+(are_all_facts_true fact rule)
