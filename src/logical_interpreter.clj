cc1aac86b1a60965d32379b42a8a78fd7f6b59d8
Federico Rulli <fede.rulli@hotmail.com>
2017-09-03 16:03:20 -0300

    aproximacion

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -36,49 +36,44 @@
 
 (defrecord Rule [name variables facts])
 
-(defmulti match_to (fn [this other] [(class this) (class other)]))
+(comment "
+(defmulti match_to (fn [this other db] [(class this) (class other) (class db)]))
 
-(defmethod match_to [Fact Fact] [this other] (= this other))
+(defmethod match_to [Fact Fact clojure.lang.PersistentVector] [this other db] (= this other))
 
-(defmethod match_to [Fact Rule] [this other]
+(defmethod match_to [Fact Rule clojure.lang.PersistentVector] [this other db]
+    (if (not= (:name this) (:name other))
+      false
+      (if (not= (count (:variables this)) (count (:variables other)))
+        false
+        (are_all_facts_true this other)
+      )
+    )
+)"
+         )
+
+(defmulti match (fn [this other db] [(class this) (class other) (class db)]))
+(defmethod match [Fact Fact clojure.lang.PersistentVector] [this other db] (= this other))
+(defmethod match [Fact Rule clojure.lang.PersistentVector] [this other db]
            (if (not= (:name this) (:name other))
              false
              (if (not= (count (:variables this)) (count (:variables other)))
                false
-               (are_all_facts_true this other)
+               (are_all_facts_true this other db)
                )
              )
            )
 
 (defn is_the_fact_true [fact list_of_facts_and_rules]
-      (if (= nil (some #(match_to fact %) list_of_facts_and_rules))
+      (if (= nil (some #(match fact % []) list_of_facts_and_rules))
         false
         true
         )
       )
 
-(defn are_all_facts_true [fact rule]
-      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-      (def database "varon(juan).
-  	varon(pepe).
-  	varon(hector).
-  	varon(roberto).
-  	varon(alejandro).
-  	mujer(maria).
-  	mujer(cecilia).
-  	padre(juan, pepe).
-  	padre(juan, pepa).
-  	padre(hector, maria).
-  	padre(roberto, alejandro).
-  	padre(roberto, cecilia).
-  	hijo(X, Y) :- varon(X), padre(Y, X).
-  	hija(X, Y) :- mujer(X), padre(Y, X).")
-      (def list_facts_and_rules (clojure.string/split-lines database))
-      (def list_facts_and_rules (map remove_tabs_dots_and_white_spaces list_facts_and_rules))
-      (def list_facts_and_rules (map create_fact_or_rule list_facts_and_rules))
-      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn are_all_facts_true [fact rule db]
       (def facts (get_facts_list (:facts rule) (zipmap (:variables rule) (:variables fact))))
-      (every? #(= true %) (map #(is_the_fact_true % list_facts_and_rules) facts))
+      (every? #(= true %) (map #(is_the_fact_true % db) facts))
       )
 
 (defn remove_tabs_dots_and_white_spaces[rule_or_fact]
@@ -122,18 +117,15 @@
        either input can't be parsed, returns nil"
       (def list_facts_and_rules (clojure.string/split-lines database))
       (def list_facts_and_rules (map remove_tabs_dots_and_white_spaces list_facts_and_rules))
-      (def list_facts_and_rules (map create_fact_or_rule list_facts_and_rules))
-      (if (= nil (some #(match_to (create_fact_or_rule query) %) list_facts_and_rules))
+      (def list_facts_and_rules  (into [] (doall (map create_fact_or_rule list_facts_and_rules))))
+      (if (= nil (some #(match (create_fact_or_rule query) % list_facts_and_rules) list_facts_and_rules))
         false
         true
         )
       )
 
-;(evaluate-query parent-database "hijo(pepe, juan)")
-(def factstring "hija(cecilia,roberto)")
-(def fact (new Fact (get_name factstring) (get_variables factstring)))
-(def rulestring "hija(X,Y):-mujer(X),padre(Y,X)")
-(def rule (new Rule(get_name rulestring) (get_variables rulestring) (get_facts_string rulestring)))
+(evaluate-query parent-database "hija(maria,roberto)")
 
 
-(are_all_facts_true fact rule)
+;(match fact fact [])
+;(are_all_facts_true fact rule)
